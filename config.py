9a28ee0207dcc20d347c05b104a9b807a35ed7a2
Rolly Maulana Awangga <rolly@awang.ga>
2017-09-07 08:01:49 +0700

    configurable

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python
 
-csvfile="dataset.csv"
+datasetinput="dataset.csv"
+
+node_size=100
+
+graphoutputfilename="graph.png"
+
+degree_centrality_filename="dc.csv"
+closeness_centrality_filename="cc.csv"
+betweenness_centrality_filename="bc.csv"
--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import matplotlib.pyplot as plt
 
 
 G=nx.Graph()
-with open(config.csvfile, newline='') as dataset:
+with open(config.datasetinput, newline='') as dataset:
 	alldata = csv.reader(dataset, delimiter=',',quotechar="|")
 	for row in alldata:
 		G.add_edge(row[0],row[1],weight=float(row[5]))
-		#print(row[0],row[1],int(row[5]))
+
 
 satu=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] >=1]
 kosong=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] <=0]
@@ -20,11 +20,10 @@ tiga=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] >=3]
 
 pos=nx.spring_layout(G)
 # nodes
-nx.draw_networkx_nodes(G,pos,node_size=100)
+nx.draw_networkx_nodes(G,pos,node_size=config.node_size)
 # edges
-
 nx.draw_networkx_edges(G,pos,edgelist=kosong,
-                    width=0.01,alpha=0.5,edge_color='b',style='dashed')
+                    width=0.1,alpha=0.5,edge_color='b',style='dashed')
 nx.draw_networkx_edges(G,pos,edgelist=satu,
                     width=1)
 nx.draw_networkx_edges(G,pos,edgelist=dua,
@@ -36,17 +35,17 @@ nx.draw_networkx_edges(G,pos,edgelist=tiga,
 # labels
 nx.draw_networkx_labels(G,pos,font_size=9,font_family='sans-serif')
 
-with open('dc.csv', 'w', newline='') as csvfile:
+with open(config.degree_centrality_filename, 'w', newline='') as csvfile:
 	spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
 	for x,y in nx.degree_centrality(G).items():
 		spamwriter.writerow([x, y])
 
-with open('cc.csv', 'w', newline='') as csvfile:
+with open(config.closeness_centrality_filename, 'w', newline='') as csvfile:
 	spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
 	for x,y in nx.closeness_centrality(G).items():
 		spamwriter.writerow([x, y])
 		
-with open('bc.csv', 'w', newline='') as csvfile:
+with open(config.betweenness_centrality_filename, 'w', newline='') as csvfile:
 	spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
 	for x,y in nx.betweenness_centrality(G).items():
 		spamwriter.writerow([x, y])
@@ -55,4 +54,4 @@ with open('bc.csv', 'w', newline='') as csvfile:
 
 
 plt.axis('off')
-plt.savefig("graph.png")
+plt.savefig(config.graphoutputfilename)
